17962dc95d9dc71d5904a4a833b3666325c72446
wangcygithub <126133363+wangcygithub@users.noreply.github.com>
2025-06-22 09:46:51 +0000

    Deploying to gh-pages from @ wangcygithub/wangcygithub.github.io@29e0a2f39901b96819daeaf1f986b13dbd92ffb5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,14 +447,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
