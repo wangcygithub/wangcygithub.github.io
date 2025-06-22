4b1b1f4d78f296dee36554ddb87d9ed20356620e
wangcygithub <126133363+wangcygithub@users.noreply.github.com>
2025-06-22 09:51:58 +0000

    Deploying to gh-pages from @ wangcygithub/wangcygithub.github.io@018d38b8f37c98b686c49403b4dbd6c01553cf8b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,7 +447,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
+            },},{id: "news-boom-ye-wu-was-awarded-by-distinguished-professor-of-jiangsu-province",
+          title: ':boom: Ye Wu was awarded by Distinguished Professor of Jiangsu Province',
+          description: "",
+          section: "News",},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
